3f2dc254727d836b36d536c451175e40ef7b2acb
Carolina Laguna <114037038+carolinaLagunaSV@users.noreply.github.com>
2023-11-08 21:34:41 -0500

    7. Modelo para form

--- 09-heroesApp/src/app/pages/heroe/heroe.component.ts
+++ 09-heroesApp/src/app/pages/heroe/heroe.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { HeroeModel } from 'src/app/models/heroe.model';
 
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
   styleUrls: ['./heroe.component.css']
 })
-export class HeroeComponent {
+export class HeroeComponent implements OnInit {
+
+  heroe = new HeroeModel();
+
+
+
+constructor(){}
+
+ngOnInit() {
 
 }
+
+guardar(form: NgForm){
+
+  if(form.invalid){
+console.log('Formulario no valido');
+return;
+
+  }
+
+console.log(form);
+console.log(this.heroe);
+}
+
+}
+
+
+
+
+
+
+
+
