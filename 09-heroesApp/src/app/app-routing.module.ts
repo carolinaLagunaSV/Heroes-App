9f8306c049651d978e6d994713e0ff269cf552a6
Carolina Laguna <114037038+carolinaLagunaSV@users.noreply.github.com>
2023-11-08 20:20:04 -0500

    5.Diseño de la pagina heroes

--- 09-heroesApp/src/app/app-routing.module.ts
+++ 09-heroesApp/src/app/app-routing.module.ts
@@ -4,8 +4,8 @@ import { HeroesComponent } from './pages/heroes/heroes.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
 
 const routes: Routes=[
-{ path:'heroes', component:HeroesComponent },
-{ path:'heroe/id', component:HeroeComponent},
+{ path:'heroes', component: HeroesComponent },
+{ path:'heroe/:id', component: HeroeComponent},
 { path:'**', pathMatch:'full', redirectTo:'heroes'},
 
 
